f4c21cb77a22fae922088a190ada6871bc30835f
Maxim <purplehorrorrus@gmail.com>
2021-02-14 15:30:41 +0800

    Minimifty code
    Left single method that set low priority

--- addon.cc
+++ addon.cc
@@ -1,8 +1,3 @@
-/*
-    Переедем на более свежую версию NODE_MODULE_VERSION сразу же, 
-    как выйдет Electron 12.0
-*/
-
 #include <node.h>
 #include <windows.h>
 #include <tlhelp32.h>
@@ -10,54 +5,44 @@
 
 namespace OverlayAddon
 {
-    using v8::Array;
     using v8::Context;
     using v8::FunctionCallbackInfo;
     using v8::Isolate;
     using v8::Local;
-    using v8::NewStringType;
-    using v8::Number;
     using v8::Object;
     using v8::String;
     using v8::Value;
 
-    void ProcessList(const FunctionCallbackInfo<Value> &args)
+    void SetLowPriority(const DWORD pid)
     {
-        Isolate *isolate = args.GetIsolate();
-        String::Utf8Value name(isolate, args[0]);
-
-        Local<Context> context = isolate->GetCurrentContext();
-        Local<Array> arr = Array::New(isolate);
-
-        HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-        PROCESSENTRY32 entry;
-
-        int i = 0;
-        while (Process32Next(snap, &entry))
-            if (!stricmp(entry.szExeFile, *name))
-                arr->Set(context, i++, Number::New(isolate, entry.th32ProcessID));
-
-        CloseHandle(snap);
-        args.GetReturnValue().Set(arr);
-    }
-
-    void SetLowPriority(const FunctionCallbackInfo<Value> &args)
-    {
-        Isolate *isolate = args.GetIsolate();
-        String::Utf8Value pid(isolate, args[0]);
-
-        if (HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, true, strtol(*pid, 0, 0)))
+        if (const HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, true, pid))
         {
             SetProcessAffinityMask(handle, (DWORD_PTR) 0x1 << (std::thread::hardware_concurrency() - 1));
-            while (!SetPriorityClass(handle, 0x00000040));
+            while (!SetPriorityClass(handle, 0x40));
             CloseHandle(handle);
         }
     }
 
-    void Initialize(Local<Object> exports)
+    void Run(const FunctionCallbackInfo<Value> &args)
     {
-        NODE_SET_METHOD(exports, "ProcessList", ProcessList);
-        NODE_SET_METHOD(exports, "SetLowPriority", SetLowPriority);
+        Isolate *isolate = args.GetIsolate();
+        const String::Utf8Value name(isolate, args[0]);
+
+        const Local<Context> context = isolate->GetCurrentContext();
+
+        const HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        PROCESSENTRY32 entry;
+
+        while (Process32Next(snap, &entry))
+            if (stricmp(entry.szExeFile, *name) == 0 && entry.pcPriClassBase != 4)
+                SetLowPriority(entry.th32ProcessID);
+                
+        CloseHandle(snap);
+    }
+
+    void Initialize(const Local<Object> exports)
+    {
+        NODE_SET_METHOD(exports, "Run", Run);
     }
 
     NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
