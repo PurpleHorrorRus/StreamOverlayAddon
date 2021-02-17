15f2a08a623735e928be9656ab96340b434312a2
Maxim <purplehorrorrus@gmail.com>
2021-02-17 09:36:20 +0800

    Remove unused code

--- addon.cc
+++ addon.cc
@@ -5,7 +5,6 @@
 
 namespace OverlayAddon
 {
-    using v8::Context;
     using v8::FunctionCallbackInfo;
     using v8::Isolate;
     using v8::Local;
@@ -15,22 +14,19 @@ namespace OverlayAddon
 
     void Run(const FunctionCallbackInfo<Value> &args)
     {
-        Isolate *isolate = args.GetIsolate();
-        const String::Utf8Value name(isolate, args[0]);
-        const Local<Context> context = isolate->GetCurrentContext();
+        const String::Utf8Value name(args.GetIsolate(), args[0]);
         const HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
         const DWORD mask = 0x1 << (std::thread::hardware_concurrency() - 1);
         PROCESSENTRY32 entry;
         
         while (Process32Next(snap, &entry))
-            if (stricmp(entry.szExeFile, *name) == 0 && entry.pcPriClassBase != 4) {
+            if (strcmp(entry.szExeFile, *name) == 0 && entry.pcPriClassBase != 4)
                 if (const HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, true, entry.th32ProcessID))
                 {
                     SetProcessAffinityMask(handle, mask);
                     while (!SetPriorityClass(handle, 0x40));
                     CloseHandle(handle);
                 }
-            }
                 
         CloseHandle(snap);
     }
